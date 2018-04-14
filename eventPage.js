9e39bed2b9b3517749f62162ff8720d6a720895f
Nicolas <nicolas.dethore@gmail.com>
2018-04-14 23:20:26 +0900

    basic chrome background page that keeps traks of one window's tab state

---
+++ eventPage.js
@@ -0,0 +1,76 @@
+var windowId = -1;
+var openTabs = [];
+
+function initialize(_) {
+	windowId = chrome.windows.WINDOW_ID_CURRENT;
+
+	console.log("Loading open tabs...");
+	chrome.tabs.query({'windowId': windowId}, function(tabs) {
+		if (tabs.length > 0) {
+			console.log("Restoring tabs!");
+
+		}
+		openTabs = tabs;
+	});
+}
+
+function onTabCreated(tab) {
+	console.log(`Tab ${tab.id} created.`);
+	openTabs.push(tab);
+}
+
+function onTabAttached(tabId, attachInfo) {
+	console.log(`Tab ${tabId} attached.`);
+	if (attachInfo.windowId == windowId) {
+		chrome.tabs.get(tabId, function(tab) {
+			openTabs.splice(attachInfo.newPosition, 0, tab);
+		});
+	}
+}
+
+function onTabMoved(tabId, moveInfo) {
+	console.log(`Tab ${tabId} moved.`);
+	if (moveInfo.windowId == windowId) {
+		var tab = openTabs.splice(moveInfo.fromIndex, 1);
+		openTabs.splice(moveInfo.toIndex, 0, tab);
+	}
+}
+
+function onTabUpdated(tabId, changeInfo, tab) {
+	if (tab.windowId == windowId) {
+		openTabs[tab.index] = tab;
+	}
+}
+
+function onTabDetached(tabId, detachInfo) {
+	console.log(`Tab ${tabId} detached.`);
+	if (detachInfo.oldWindowId == windowId) {
+		openTabs.splice(detachInfo.ldPosition, 1);
+	}
+}
+
+function onTabRemoved(tabId, removeInfo) {
+	console.log(`Tab ${tabId} removed.`);
+	if (removeInfo.windowId == windowId) {
+		chrome.tabs.get(tabId, function(tab) {
+			let index = openTabs.indexOf(tab);
+			if (index != -1) {
+				openTabs.splice(index, 1);
+			}
+		});
+	}
+}
+
+console.log("Starting up...");
+if (chrome.runtime && chrome.runtime.onStartup) {
+	chrome.runtime.onInstalled.addListener(initialize);
+	chrome.runtime.onStartup.addListener(initialize);
+	chrome.tabs.onCreated.addListener(onTabCreated);
+	chrome.tabs.onAttached.addListener(onTabAttached);
+	chrome.tabs.onMoved.addListener(onTabMoved);
+	chrome.tabs.onDetached.addListener(onTabDetached);
+	chrome.tabs.onRemoved.addListener(onTabRemoved);
+}
+else {
+	console.log("This extension requires Chrome 23 or above. Please update Chrome and retry.");
+}
