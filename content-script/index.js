f80bb51f18ae1b81f541fec4d269dfe307f03bd2
Nicolas <nicolas.dethore@gmail.com>
2018-04-19 19:35:13 +0900

    re-implement slot mechanism for content script injection

--- content-script/index.js
+++ content-script/index.js
@@ -5,8 +5,10 @@ var App = require("./components/App")
 ** Render component in a placeholder div to avoid overriding
 ** existing DOM elements.
 */
-var	container = document.createElement("div");
-
-m.mount(container, App);
-
-document.body.appendChild(container.firstElementChild);
+var	slot = document.getElementById("swoop-slot");
+if (!slot) {
+	slot = document.createElement("div");
+	slot.setAttribute("id", "swoop-slot");
+	document.body.appendChild(slot);
+}
+m.mount(slot, App);
